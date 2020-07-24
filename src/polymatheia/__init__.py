bc5dcae0100ade53ac42f7b93b8873e3194dcba2
Mark Hall <mark.hall@work.room3b.eu>
2020-07-24 16:21:05 +0100

    Added basic infrastructure

---
+++ src/polymatheia/__init__.py
@@ -0,0 +1,10 @@
+"""This is the main package for the Polymatheia library."""
+
+
+def placeholder():
+    """Return a placeholder text.
+
+    :return: The string ``'<<placeholder>>'``
+    :rtype: ``string``
+    """
+    return "<<placeholder>>"
