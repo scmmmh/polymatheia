5ebd14906250520d3f6ffe0ddbd1078e0be7076d
Mark Hall <mark.hall@work.room3b.eu>
2020-07-30 15:39:43 +0100

    Added a CSV writer

--- src/polymatheia/data/writer.py
+++ src/polymatheia/data/writer.py
@@ -3,6 +3,7 @@ import json
 import os
 
 from copy import copy
+from csv import DictWriter
 
 from polymatheia.util import identifier_to_directory_structure
 
@@ -28,7 +29,11 @@ class LocalWriter():
             self._id_path = id_path
 
     def write(self, records):
-        """Write the records to the file-system."""
+        """Write the records to the file-system.
+
+        :param records: The records to write
+        :type records: Iterable of :class:`~polymatheia.data.NavigableDict`
+        """
         for record in records:
             path = copy(self._id_path)
             identifier = record
@@ -43,3 +48,60 @@ class LocalWriter():
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             with open(f'{file_path}.json', 'w') as out_f:
                 json.dump(record, out_f)
+
+
+class CSVWriter():
+    """The :class:`~polymatheia.data.writer.CSVWriter` writes records into a CSV file.
+
+    The :class:`~polymatheia.data.writer.CSVWriter` assumes that no record contains any kind of nested data. If
+    it is passed nested data, then the behaviour is undefined.
+    """
+
+    def __init__(self, target, default_value='', extras_action='ignore', column_names=None):
+        """Create a new :class:`~polymatheia.data.writer.CSVWriter`.
+
+        :param target: The target to write the CSV to. Can either be a ``str`` filename or an existing file-like object
+        :param default_value: The default value to output if a record does not contain a value for a specified CSV
+                              column name
+        :param extras_action: The action to take if a record contains keys that are not in the CVS fieldnames. Set to
+                              ``'ignore'`` to just ignore this (the default). Set to ``'raise'`` to raise a
+                              ``ValueError`.
+        :type extras_action: ``str``
+        :param fieldnames: The CSV column names to use. If ``None`` is specified, then the column names are derived
+                           from the first record's keys.
+        :type fieldnames: ``list`` of ``str``
+        """
+        self._target = target
+        self._default_value = default_value
+        self._extras_action = extras_action
+        self._column_names = column_names
+
+    def write(self, records):
+        """Write the ``records`` to the CSV file.
+
+        :param records: The records to write
+        :type records: Iterable of :class:`~polymatheia.data.NavigableDict`
+        """
+        if isinstance(self._target, str):
+            with open(self._target, 'w') as out_file:
+                self._write_csv(out_file, records)
+        else:
+            self._write_csv(self._target, records)
+
+    def _write_csv(self, file, records):
+        """Perform the actual writing of the CSV file.
+
+        :param file: The file-like object to write to
+        :param records: The records to write
+        :type records: Iterable of :class:`~polymatheia.data.NavigableDict`
+        """
+        csv_writer = None
+        for record in records:
+            if not csv_writer:
+                csv_writer = DictWriter(file,
+                                        fieldnames=record.keys() if self._column_names is None
+                                        else self._column_names,
+                                        restval=self._default_value,
+                                        extrasaction=self._extras_action)
+                csv_writer.writeheader()
+            csv_writer.writerow(record)
---
+++ tests/data_tests/writer_tests/csv_writer_test.py
@@ -0,0 +1,53 @@
+"""Tests for the :class:`~polymatheia.data.writer.CSVWriter`."""
+import os
+
+from csv import DictReader
+from io import StringIO
+from shutil import rmtree
+
+from polymatheia.data.reader import LocalReader
+from polymatheia.data.writer import CSVWriter
+from polymatheia.transform import RecordsTransform
+
+
+def test_csv_file_writing():
+    """Test writing records to a local filename."""
+    reader = LocalReader('tests/fixtures/local_reader_test')
+    transform = RecordsTransform(reader, [
+        ('copy', 'id', 'header.identifier._text'),
+        ('copy', 'set', 'header.setSpec._text'),
+        ('copy', 'title', ['metadata', '{http://www.openarchives.org/OAI/2.0/oai_dc/}dc', 'dc_title', '_text'])
+    ])
+    rmtree('tmp/csv_writer_test.csv', ignore_errors=True)
+    writer = CSVWriter('tmp/csv_writer_test.csv')
+    writer.write(transform)
+    assert os.path.exists('tmp/csv_writer_test.csv')
+    count = 0
+    with open('tmp/csv_writer_test.csv') as in_file:
+        for line in DictReader(in_file):
+            assert 'id' in line
+            assert 'set' in line
+            assert 'title' in line
+            count = count + 1
+    assert count == 10
+
+
+def test_csv_in_memory_writing():
+    """Test that writing records to an existing file-like object works."""
+    reader = LocalReader('tests/fixtures/local_reader_test')
+    transform = RecordsTransform(reader, [
+        ('copy', 'id', 'header.identifier._text'),
+        ('copy', 'set', 'header.setSpec._text'),
+        ('copy', 'title', ['metadata', '{http://www.openarchives.org/OAI/2.0/oai_dc/}dc', 'dc_title', '_text'])
+    ])
+    buffer = StringIO()
+    writer = CSVWriter(buffer)
+    writer.write(transform)
+    buffer.seek(0)
+    count = 0
+    for line in DictReader(buffer):
+        assert 'id' in line
+        assert 'set' in line
+        assert 'title' in line
+        count = count + 1
+    assert count == 10
