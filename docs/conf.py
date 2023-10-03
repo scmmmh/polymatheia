41dfd4f737d59e2fa6eb00eee1ba1664d898cad4
Mark Hall <mark.hall@work.room3b.eu>
2023-10-03 10:01:36 +0100

    v1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Mark Hall'
 author = 'Mark Hall'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.4'
+release = '1.0.5'
 
 
 # -- General configuration ---------------------------------------------------
